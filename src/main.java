2e8cc97bc47482cfe5c5d71067ae26d31d8071ab
Laurent De Laere <laurent.delaere@delacom.be>
2016-03-16 08:32:56 +0100

    Dit is een nuttige aanpassing.

--- src/main.java
+++ src/main.java
@@ -70,6 +70,8 @@ public class main {
 		DHCPClient client = new DHCPClient();
 		client.getIP();
 		
+		System.out.println("AANPASSING");
+		
 		
 //		DatagramSocket socket = new DatagramSocket();
 //		Message response = client.DHCPDiscover(socket);
