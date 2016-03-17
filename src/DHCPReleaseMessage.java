cd7564dd6df2308f487c2f5c2831ff813098a8cb
Simon Geirnaert <r0461511@durbuy.cs.kotnet.kuleuven.be>
2016-03-17 12:34:06 +0100

    new subclass release message

---
+++ src/DHCPReleaseMessage.java
@@ -0,0 +1,27 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Class representing a DHCP release message.
+ * 
+ * @author Laurent De Laere
+ * 		   Simon Geirnaert
+ *
+ */
+public class DHCPReleaseMessage extends Message {
+	
+	/**
+	 * Constant representing the flag to receive the DHCPOFFER in unicast
+	 */
+	private static final int FLAGS0 = 0;
+	
+	/**
+	 * Intialize the new DHCP release message with given MAC address
+	 * @throws UnknownHostException 
+	 * @throws IllegalArgumentException 
+	 * 
+	 */
+	public DHCPReleaseMessage(String macAddress) throws IllegalArgumentException, UnknownHostException {
+		super(1,1,6,0, Utilities.generateXid(), 0, FLAGS0, InetAddress.getByName("0.0.0.0"), InetAddress.getByName("0.0.0.0"), InetAddress.getByName("0.0.0.0"), InetAddress.getByName("0.0.0.0"), macAddress, "", "",  new OptionsList());
+	}
+}
