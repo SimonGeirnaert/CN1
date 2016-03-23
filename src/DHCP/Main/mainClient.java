d666641d050470d3e6f10f26db44b3f41a186f51
Laurent De Laere <laurent.delaere@delacom.be>
2016-03-23 01:24:08 +0100

    Laatste kleine aanpassingen, requested adres van blablabla.166 weggedaan en handleResponse threaded gemaakt (hielp helaas niet om getIP's simultaan uit te voeren, ligt wss aan de getIP() maar kga slapen)

--- src/DHCP/Main/mainClient.java
+++ src/DHCP/Main/mainClient.java
@@ -4,9 +4,31 @@ import DHCP.DHCPClient;
 
 public class mainClient {
 	public static void main(String[] args) throws Exception {
+		
 		// Two different clients after each other: same IP
 		DHCPClient client = new DHCPClient("JH57DF98RV15FH96");
 		DHCPClient client2 = new DHCPClient("JH57DF98RV15FH95");
+		
+//		class ThreadClient implements Runnable {
+//			DHCPClient client;
+//			ThreadClient(DHCPClient client){
+//				this.client = client;
+//			}
+//			public void run(){
+//				try {
+//					client.getIP();
+//				} catch (Exception e) {
+//					System.out.println("Error occured while retrieving IP.");
+//				}
+//			}
+//		}
+//		
+//		Thread t1 = new Thread(new ThreadClient(client));
+//		Thread t2 = new Thread(new ThreadClient(client2));
+//		
+//		t1.run();
+//		t2.run();
+		
 		client.getIP();
 		System.out.println("");
 		client2.getIP();
@@ -14,4 +36,5 @@ public class mainClient {
 		client2.getIP();
 	}
 
+	
 }
